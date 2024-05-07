948d1ca0683103fc3c6e47b244d05e67a75cbbed
Diego Maggessi <diegosmteixeira@gmail.com>
2024-05-07 10:17:12 -0300

    add: maxheap - insert, remove and sink_down

---
+++ 10-Heap/heap.py
@@ -0,0 +1,115 @@
+class MaxHeap:
+    def __init__(self):
+        self.heap = []
+
+    # heap starting at index 0 helper methods:
+    def _left_child(self, index):
+        return 2 * index + 1
+    
+    def _right_child(self, index):
+        return 2 * index + 2
+    
+    def _parent(self, index):
+        return (index - 1) // 2
+    
+    def _swap(self, index1, index2):
+        self.heap[index1], self.heap[index2] = self.heap[index2], self.heap[index1]
+
+    def insert(self, value):
+        self.heap.append(value) # add value at the end
+        current = len(self.heap) - 1 # the index of the last value
+
+        while current > 0 and self.heap[current] > self.heap[self._parent(current)]:
+            self._swap(current, self._parent(current))
+            current = self._parent(current)
+
+    def remove(self):
+        if len(self.heap) == 0:
+            return None
+        
+        if len(self.heap) == 1:
+            return self.heap.pop()
+        
+        max_value = self.heap[0]
+
+        # take the last item within the list and move down to the first position
+        self.heap[0] = self.heap.pop()
+        self._sink_down(0)
+        
+        return max_value
+    
+    def _sink_down(self, index):
+        max_index = index
+
+        while True:
+            left_index = self._left_child(index)
+            right_index = self._right_child(index)
+
+            # any index equal or greater than the length of the heap is not valid
+            if (left_index < len(self.heap) 
+                    and self.heap[left_index] > self.heap[max_index]):
+                max_index = left_index
+            
+            # any index equal or greater than the length of the heap is not valid
+            if (right_index < len(self.heap) and 
+                    self.heap[right_index] > self.heap[max_index]):
+                max_index = right_index
+
+            if max_index != index:
+                self._swap(index, max_index)
+                index = max_index
+            else:
+                return
+
+
+myheap = MaxHeap()
+myheap.insert(99)
+myheap.insert(72)
+myheap.insert(61)
+myheap.insert(58)
+
+print(myheap.heap)
+
+myheap.insert(100)
+
+print(myheap.heap)
+
+myheap.insert(75)
+
+print(myheap.heap)
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    [99, 72, 61, 58]
+    [100, 99, 61, 58, 72]
+    [100, 99, 75, 58, 72, 61]
+"""
+print('\n')
+
+myheap = MaxHeap()
+myheap.insert(95)
+myheap.insert(75)
+myheap.insert(80)
+myheap.insert(55)
+myheap.insert(60)
+myheap.insert(50)
+myheap.insert(65)
+
+print(myheap.heap)
+
+myheap.remove()
+
+print(myheap.heap)
+
+myheap.remove()
+
+print(myheap.heap)
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    [95, 75, 80, 55, 60, 50, 65]
+    [80, 75, 65, 55, 60, 50]
+    [75, 60, 65, 55, 50]
+"""
